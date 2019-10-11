399d34dff8816d9d3cfbfff5750e3930a1a98df3
Jonathan Van Dam <mustafolins@gmail.com>
2019-10-11 00:43:46 -0600

    Added documentation

--- FindJpgs/Program.cs
+++ FindJpgs/Program.cs
@@ -11,25 +11,35 @@ namespace FindJpgs
 
         static void Main(string[] args)
         {
+            // todo: should probably have this be one of the command line arguments
             var file = "/home/mustafolins/Documents/Classes/CSIS 3700/GoblinsV2.dd";
             using (var fs = new FileStream(file, FileMode.Open))
             {
+                // the header of the type of file being looked for doesn't have to be
+                // jpgs it should work for other file types as well.
                 byte[] header = { 0xFF, 0xD8, 0xFF };   // leaving off the last byte of the header
                                                         // in case it's a different version
+                // the footer of the type of file to be looked for.
                 byte[] footer = { 0xFF, 0xD9 };
 
+                // indicates that the start of the file has been reached
                 bool startOfFile = false;
 
+                // initialize list for temporary storage of bytes
                 List<byte> readBytes = new List<byte>();
+                // loop through entire file to find all potential files
                 for (int i = 0; i < fs.Length; i++)
                 {
+                    // read a byte
                     readBytes.Add((byte)fs.ReadByte());
+                    // start of file reached
                     if (!startOfFile && EndsWithArray(readBytes, header))
                     {
                         readBytes.Clear();
                         readBytes.AddRange(header);
                         startOfFile = true;
                     }
+                    // foor of file reached
                     else if (EndsWithArray(readBytes, footer))
                     {
                         WriteFile(readBytes);
@@ -40,6 +50,8 @@ namespace FindJpgs
             }
         }
 
+        ///<summary>Writes a file with the given <paramref name="readBytes"/></summary>
+        ///<param name="readBytes">The bytes to write to file.</param>
         private static void WriteFile(List<byte> readBytes)
         {
             using (var fs = new FileStream("Goblin" + CurrentCount + ".jpg", FileMode.OpenOrCreate))
@@ -55,6 +67,7 @@ namespace FindJpgs
             CurrentCount++;
         }
 
+        ///<summary>Returns true if the given bytes list ends with the given byteArray.</summary>
         private static bool EndsWithArray(List<byte> bytes, byte[] byteArray)
         {
             if (bytes.Count < byteArray.Length)
