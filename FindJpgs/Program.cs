98748e9a8984590bfb79e6e5aaea77eee74ef83d
Jonathan Van Dam <mustafolins@gmail.com>
2019-10-12 01:52:54 -0600

    Added async call
    Added async call to write file so main loop can continue looking for files.  Changed program to take reading file and output filename as command line arguments.

--- FindJpgs/Program.cs
+++ FindJpgs/Program.cs
@@ -2,24 +2,34 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace FindJpgs
 {
     class Program
     {
-        private static int CurrentCount = 1;
+        public static int CurrentCount { get; set; } = 1;
+        public static string FileName { get; set; } = "";
+        public static int WritingToFileCount { get; set; } = 0;
 
         static void Main(string[] args)
         {
-            // todo: should probably have this be one of the command line arguments
-            var file = "/home/mustafolins/Documents/Classes/CSIS 3700/GoblinsV2.dd";
+            if (args.Length < 1)
+            {
+                Console.WriteLine($"Usage: FindJpgs InFile OutputFileName");
+                return;
+            }
+
+            var file = args[0];
+            FileName = args?[1] ?? "";
             using (var fs = new FileStream(file, FileMode.Open))
             {
                 // the header of the type of file being looked for doesn't have to be
                 // jpgs it should work for other file types as well.
                 byte[] header = { 0xFF, 0xD8, 0xFF };   // leaving off the last byte of the header
                                                         // in case it's a different version
-                // the footer of the type of file to be looked for.
+                                                        // the footer of the type of file to be looked for.
                 byte[] footer = { 0xFF, 0xD9 };
 
                 // indicates that the start of the file has been reached
@@ -42,29 +52,37 @@ namespace FindJpgs
                     // foor of file reached
                     else if (EndsWithArray(readBytes, footer))
                     {
-                        WriteFile(readBytes);
+                        _ = WriteFile(readBytes.ToArray());
                         readBytes.Clear();
                         startOfFile = false;
                     }
+                }
+
+                while (WritingToFileCount > 0)
+                {
+                    Thread.Sleep(1);
                 }
             }
         }
 
         ///<summary>Writes a file with the given <paramref name="readBytes"/></summary>
         ///<param name="readBytes">The bytes to write to file.</param>
-        private static void WriteFile(List<byte> readBytes)
+        private static async Task WriteFile(byte[] readBytes)
         {
-            using (var fs = new FileStream("Goblin" + CurrentCount + ".jpg", FileMode.OpenOrCreate))
+            WritingToFileCount++;
+            await Task.Delay(1);
+            using (var fs = new FileStream(FileName + CurrentCount + ".jpg", FileMode.OpenOrCreate))
             {
                 using (var bw = new BinaryWriter(fs))
                 {
-                    for (int i = 0; i < readBytes.Count; i++)
+                    for (int i = 0; i < readBytes.Length; i++)
                     {
                         bw.Write(readBytes[i]);
                     }
                 }
             }
             CurrentCount++;
+            WritingToFileCount--;
         }
 
         ///<summary>Returns true if the given bytes list ends with the given byteArray.</summary>
